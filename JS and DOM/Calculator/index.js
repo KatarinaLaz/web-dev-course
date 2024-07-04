798e1f61f533581e123198d4aff416f248c5fd4e
katarina.lazic <katarinalazic.kata@gmail.com>
2024-07-04 09:44:04 +0200

    finished calculator

--- JS and DOM/Calculator/index.js
+++ JS and DOM/Calculator/index.js
@@ -8,8 +8,8 @@ var seven = document.querySelector("#seven");
 var eight = document.querySelector("#eight");
 var nine = document.querySelector("#nine");
 var zero = document.querySelector("#zero");
-var divide = document.querySelector("#divide");
-var multiply = document.querySelector("#multiply");
+var divi = document.querySelector("#divi");
+var multi = document.querySelector("#multi");
 var minus = document.querySelector("#minus");
 var plus = document.querySelector("#plus");
 var ce = document.querySelector("#ce");
@@ -23,55 +23,80 @@ var saves = document.querySelectorAll(".save");
 
 var num1 = 0;
 var num2 = 0;
-var operator = "";
+var operate;
 
 for(const num of nums) {
     num.addEventListener("click", function() {
         if (input.value == 0) {
-            input.value = "";
-        }
-        input.value = input.value + num.innerHTML;
-    });
-}
-
-for(var save of saves) {
-    save.addEventListener("click", function () {
-        num1 = input.value;
-
-        if(save.id == "divide") {
-            operator = divide;
-        } else if (save.id == "multiply") {
-            operator = multiply;
-        } else if (save.id == "minus") {
-            operator = substract;
+            input.value = num.innerHTML;
         } else {
-            operator = add;
+            input.value = input.value + num.innerHTML;
         }
-
-        input.value = 0;
     });
 }
+
+divi.addEventListener("click", function() {
+    num1 = input.value;
+
+    operate = divide;
+
+    input.value = 0;
+});
+
+multi.addEventListener("click", function() {
+    num1 = input.value;
+
+    operate = multiply;
+
+    input.value = 0;
+});
+
+plus.addEventListener("click", function() {
+    num1 = input.value;
+
+    operate = add;
+
+    input.value = 0;
+});
+
+minus.addEventListener("click", function() {
+    num1 = input.value;
+
+    operate = substract;
+
+    input.value = 0;
+});
+
+back.addEventListener("click", function() {
+    input.value = Math.floor(input.value / 10);
+});
 
 equals.addEventListener("click", function() {
     num2 = input.value;
 
-    input.value = calculate(num1, num2, operator);
+    input.value = calculate(num1, num2, operate);
+});
+
+ce.addEventListener("click", function () {
+    input.value = 0;
+    num1 = 0;
+    num2 = 0
 });
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function substract(num1, num2) {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 function calculate(num1, num2, operator) {
