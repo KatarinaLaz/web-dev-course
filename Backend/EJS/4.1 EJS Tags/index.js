7e898c26b0ecb1e16c6d5a41376bfb43916c261a
katarina.lazic <katarinalazic.kata@gmail.com>
2025-05-30 13:23:49 +0200

    ejs start

--- Backend/EJS/4.1 EJS Tags/index.js
+++ Backend/EJS/4.1 EJS Tags/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express'
 const app = express();
 const port = 3000;
 
@@ -15,3 +15,13 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+/**
+ *  <%= variable %>                 JS Output                 - interpreted as JS and creates an output
+ *  <% console.log("Hello") %>      JS Execute                - executable JS code inside the HTML - doesn't give output, it's like Void
+ *  <%- <h1>Hello</h1> %>           Render HTML               - everything inside the tag is interpreted as HTML
+ *  <%%  %%>                        Show <% or %>             - a way to actually write <% or %> without it being interpreted as tags
+ *  <%# This is a comment %>        stop Execution            - this is how to write comments inside EJS
+ *  <%- include("<EJS FILE>") %>    Insert another EJS file   - for reusing parts like header and footer (e.g. 'include("<footer.ejs>")')
+ */
