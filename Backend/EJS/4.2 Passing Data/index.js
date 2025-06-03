3f77b8580d50dbaa782918d4adb882b8118dbd81
katarina.lazic <katarinalazic.kata@gmail.com>
2025-06-03 14:58:29 +0200

    EJS

--- Backend/EJS/4.2 Passing Data/index.js
+++ Backend/EJS/4.2 Passing Data/index.js
@@ -3,12 +3,25 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let numberOfLetters;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+function count(req, res, next) {
+  let name = req.body.fName + req.body.lName;
+  numberOfLetters = name.length;
+  next();
+}
 
-app.post("/submit", (req, res) => {});
+app.use(count);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", {numberOfLetters: numberOfLetters});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- Backend/EJS/4.3 EJS Partials/index.js
+++ Backend/EJS/4.3 EJS Partials/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));  // We put all of our static files (files that don't change) in the public folder and so that Express knows and treat them as such
+                                    // If the file is an src folder, for example, we write like this (express.static("src/public"));
+                                    // When we reference these files, the public folder becomes the starting point. Example:
+                                    // <link rel="stylesheet" href="/styles/layout.css"> => the styles folder is inside the public folder
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
@@ -9,6 +16,18 @@ Step 2: Make sure that static files are linked to and the CSS shows up.
 Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
